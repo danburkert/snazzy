f18699e704c7d9f60744c12ae7eb1e49d7457501
Dan Burkert <dan@danburkert.com>
2017-09-10 11:29:24 -0700

    Update for prost 0.2

--- build.rs
+++ build.rs
@@ -2,6 +2,5 @@ extern crate prost_build;
 
 fn main() {
     prost_build::compile_protos(&["src/items.proto"],
-                                &["src/"],
-                                None).unwrap();
+                                &["src/"]).unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,13 @@ extern crate prost;
 #[macro_use]
 extern crate prost_derive;
 
+use std::io::Cursor;
+
+use prost::Message;
+
 // Include the `items` module, which is generated from items.proto.
 pub mod items {
-    include!(concat!(env!("OUT_DIR"), "/items.rs"));
+    include!(concat!(env!("OUT_DIR"), "/snazzy.items.rs"));
 }
 
 pub fn create_large_shirt(color: String) -> items::Shirt {
@@ -12,4 +16,16 @@ pub fn create_large_shirt(color: String) -> items::Shirt {
     shirt.color = color;
     shirt.set_size(items::shirt::Size::Large);
     shirt
+}
+
+pub fn serialize_shirt(shirt: &items::Shirt) -> Vec<u8> {
+    let mut buf = Vec::new();
+    buf.reserve(shirt.encoded_len());
+    // Unwrap is safe, since we have reserved sufficient capacity in the vector.
+    shirt.encode(&mut buf).unwrap();
+    buf
+}
+
+pub fn deserialize_shirt(buf: &[u8]) -> Result<items::Shirt, prost::DecodeError> {
+    items::Shirt::decode(&mut Cursor::new(buf))
 }
